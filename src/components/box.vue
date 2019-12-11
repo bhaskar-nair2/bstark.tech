0f2760808de7c36619b72ad9c6e8d89b78b494f5
bhaskar <b.bhaskar.nair@gmail.com>
2019-12-11 22:57:15 +0530

    shadow shift

--- src/components/box.vue
+++ src/components/box.vue
@@ -1,6 +1,6 @@
 <template>
 	<div
-		:class="`${boxType}${isMobile?'--mobile':''}`"
+		:class="`${boxType}${isMobile?'--mobile':''} ${elevated?'elevated':''}`"
 		:style="position"
 	>
 		<imgHolder
@@ -8,35 +8,37 @@
 			:typ="typ"
 			v-if="role.type=='img'"
 		></imgHolder>
-		<poemHold v-if="role.type=='poem'"></poemHold>
-		<songHold v-if="role.type=='song'"></songHold>
+		<poemHold :typ="typ" v-if="role.type=='poem'"></poemHold>
+		<songHold :typ="typ" v-if="role.type=='song'"></songHold>
 	</div>
 </template>
 
 <script>
+import { setInterval } from 'timers';
+
 export default {
 	name: 'box',
+	created() {
+		setInterval(
+			this.elevate,
+			(100 * parseFloat(this.boxData.id)) / this.boxData.id.length
+		);
+	},
 	components: {
 		imgHolder: () => import('@/components/roleHolders/imgHold'),
 		poemHold: () => import('@/components/roleHolders/poemHold'),
-		songHold: ()=> import('@/components/roleHolders/songHold')
+		songHold: () => import('@/components/roleHolders/songHold')
 	},
 	props: {
-		boxType: {
-			type: String,
-			default: 'box1'
-		},
-		pos: {
-			type: Array
-		},
-		typ: { type: String, required: true },
-		role: {
+		boxData: {
 			type: Object,
 			required: true
 		}
 	},
 	data() {
-		return {};
+		return {
+			elevated: false
+		};
 	},
 	computed: {
 		position() {
@@ -44,6 +46,26 @@ export default {
 				'grid-row': this.pos[0],
 				'grid-column': this.pos[1]
 			};
+		},
+		boxType() {
+			return this.boxData.type || 'box1';
+		},
+		pos() {
+			return this.boxData.position;
+		},
+		typ() {
+			return this.boxData.type;
+		},
+		role() {
+			return this.boxData.role;
+		}
+	},
+	methods: {
+		elevate() {
+			if (this.isMobile)
+				setTimeout(() => {
+					this.elevated = !this.elevated;
+				}, 500); // animation duration buffer
 		}
 	}
 };
