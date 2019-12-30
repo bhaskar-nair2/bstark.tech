1335ab912394cfe682ed5ceb9e1484d8b3c6c4ae
bhaskar <b.bhaskar.nair@gmail.com>
2019-12-30 10:32:35 +0530

    ordered

--- src/components/roleHolders/contactRole.vue
+++ src/components/roleHolders/contactRole.vue
@@ -1,21 +1,36 @@
 <template>
-	<div v-if="modal" class="roleCont--modal">
-		<button class="cross" @click.self="closeModal">
+	<div class="roleCont--modal" v-if="modal">
+		<button @click.self="closeModal" class="cross">
 			<v-icon
 				@click.passive="closeModal"
 				name="times"
-				scale="1"
+				scale="2"
 			></v-icon>
 		</button>
-		<v-icon class="icn" name="paper-plane" scale="3"></v-icon>
-		<form @submit.prevent="sendMail">
-			<label for="name">Name</label>
-			<input type="name" id="name" required />
-			<label for="mail">Mail Id</label>
-			<input type="email" id="mail" required />
-		</form>
+		<div class="content">
+			<div>
+				<v-icon class="icn" name="paper-plane" scale="3"></v-icon>
+			</div>
+			<form @submit.prevent="sendMail">
+				<div class="inputBox">
+					<label for="name">Name</label>
+					<input class="input" id="name" required type="name" />
+				</div>
+				<div class="inputBox">
+					<label for="mail">Mail Id</label>
+					<input class="input" id="mail" required type="email" />
+				</div>
+				<div class="inputBox">
+					<label for="msg">Message</label>
+					<textarea class="input" cols="20" id="msg" rows="6" />
+				</div>
+				<div class="sub">
+					<button type="submit">Send</button>
+				</div>
+			</form>
+		</div>
 	</div>
-	<div v-else class="roleCont">
+	<div class="roleCont" v-else>
 		<v-icon class="icn" name="paper-plane" scale="2"></v-icon>
 	</div>
 </template>
