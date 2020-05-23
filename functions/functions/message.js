d194b5f26bf69868e02bb8568b177ec2771b5a29
bhaskar <b.bhaskar.nair@gmail.com>
2020-05-23 12:03:15 +0530

    removed mail oncreate

--- functions/functions/message.js
+++
@@ -1,8 +0,0 @@
-const functions = require('firebase-functions');
-
-exports.sendMail = functions.firestore
-	.document('/messages/{messageId}')
-	.onCreate(async (snapshot, context) => {
-		const data = snapshot.data();
-		console.log(data);
-	});
--- functions/index.js
+++ functions/index.js
@@ -1,14 +1,12 @@
 const functions = require('firebase-functions');
-const admin = require('firebase-admin')
+const admin = require('firebase-admin');
 // exports.helloWorld = functions.https.onRequest((request, response) => {
 //  response.send("Hello from Firebase!");
 // });
 
-exports.sendMail = require("./functions/message")
-
-var serviceAccount = require("./service-account.json");
+var serviceAccount = require('./service-account.json');
 
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-  databaseURL: "https://bstark-tech.firebaseio.com"
+	credential: admin.credential.cert(serviceAccount),
+	databaseURL: 'https://bstark-tech.firebaseio.com'
 });
